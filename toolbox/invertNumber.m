f119de547624d7a119acec0f8a6f0db5c58f1653
Bensingh Pancras <bpancras@mathworks.com>
2025-06-19 14:51:03 +0530

    Added test for invert Number

--- toolbox/invertNumber.m
+++ toolbox/invertNumber.m
@@ -1,6 +1,6 @@
 function invertedNumber = invertNumber(numberToInvert)
 arguments
-    numberToInvert(1,:) double;
+    numberToInvert(1,:) {mustBeNumeric};
 end
 mh = mexhost();
 invertedNumber = mh.feval("invertMex", numberToInvert);
